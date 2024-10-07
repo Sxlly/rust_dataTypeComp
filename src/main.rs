55bb652e1a48af78c482c3da060b2bd1ecfbbc42
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-07 18:16:44 +0800

    integers

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,15 @@
 
 
 fn main() {
-    
+
+    //integers
+
+    let x: i32 = 20; //32 bit signed integer
+    let y: i64 = -20; //64 bit signed integer
+
+    let a: u32 = 40; //32 bit unsigned integer
+    let b: u8 = 240; //8 bit signed integer
+
+
+
 }
